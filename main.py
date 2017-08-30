728e01d768d7dc0284ce01734d335d28a835cbbe
DimitriosHytiroglou <DimitriosHytiroglou@users.noreply.github.com>
2017-08-29 22:24:34 -0700

    Change to Form

--- main.py
+++ main.py
@@ -14,11 +14,14 @@
 
 import webapp2
 
+form="""
+<form action="http://www.google.com/search">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.out.write('Hello, webapp World!')
-		
-app = webapp2.WSGIApplication([('/', MainPage)],
-			debug=True)
-
+		self.response.out.write(form)
+app = webapp2.WSGIApplication([('/',MainPage)],
+								debug=True)
